ab754c966ff90acea003faa84ee73af9404283d5
Himangshu <vistahista1@gmail.com>
2026-09-13 21:01:32 +0530

    build: attach debug signing fallback for assembleRelease packaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
             optimization {
                 enable = false
             }
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
