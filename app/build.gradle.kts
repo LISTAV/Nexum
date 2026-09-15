7a39de87dcfe2c43543effd1b6d3873140382798
Himangshu <vistahista1@gmail.com>
2026-09-15 23:39:02 +0530

    build: configure compileSdk release(37), targetSdk 35, and clean release packaging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 
 android {
     namespace = "com.kairav.nexum"
-    compileSdk = 35
+    compileSdk {
+        version = release(37)
+    }
 
     defaultConfig {
         applicationId = "com.kairav.nexum"
